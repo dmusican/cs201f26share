90819087b2b5370481d60a5d7bd1b2d3b7ee1c61
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-09-18 14:53:46 -0500

    Scripted auto-commit on change (2026-09-18 14:53:46) by gitwatch.sh

---
+++ in-class-examples/src/main/kotlin/Example2/Example2.kt
@@ -0,0 +1,21 @@
+package Example2
+
+class Salary {
+    var annual: Double? = null
+
+    fun giveRaise(percent: Double) {
+        val annualCopy = annual
+        if (annualCopy != null) {
+            annual = annualCopy + annualCopy * percent
+        } else {
+            // do nothing
+        }
+    }
+}
+
+fun main() {
+    val pay = Salary()
+//    pay.annual = 100.0
+    println(pay.annual)
+    pay.giveRaise(0.8)
+}
