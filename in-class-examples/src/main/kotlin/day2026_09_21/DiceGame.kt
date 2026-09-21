771bc80a3e82dfc33290d61ae82fe163019a85e0
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-09-21 14:38:38 -0500

    Scripted auto-commit on change (2026-09-21 14:38:38) by gitwatch.sh

--- in-class-examples/src/main/kotlin/day2026_09_21/DiceGame.kt
+++ in-class-examples/src/main/kotlin/day2026_09_21/DiceGame.kt
@@ -1,9 +1,13 @@
 package day2026_09_21
 
-class Die {
+class Die(var sides: Int) {
 
 }
 
 fun main() {
-    val die1 = Die()
+    val die1 = Die(20)
+    println(die1.sides)
+
+    val die2 = Die(12)
+    println(die2.sides)
 }
