c64503b4aaefa20c0b7b2d8da75536632fd1ac32
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-09-23 14:25:29 -0500

    Scripted auto-commit on change (2026-09-23 14:25:29) by gitwatch.sh

--- in-class-examples/src/main/kotlin/day2026_09_23/FunWithShapes.kt
+++ in-class-examples/src/main/kotlin/day2026_09_23/FunWithShapes.kt
@@ -1,2 +1,20 @@
 package day2026_09_23
 
+class Rectangle(var length: Double, var width: Double) {
+    fun area(): Double {
+        return length * width
+    }
+}
+
+class Circle(var radius: Double) {
+    fun area(): Double {
+        return Math.PI * radius * radius
+    }
+}
+
+fun main() {
+    val r = Rectangle(10.0, 10.0)
+    val c = Circle(35.0)
+    println(r.area())
+    println(c.area())
+}
