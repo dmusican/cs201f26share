443acb2a90406b536f57f6f7d72df745d328ee69
Dave Musicant (expanse) <dmusicant-expanse@carleton.edu>
2026-09-25 09:50:05 -0500

    Scripted auto-commit on change (2026-09-25 09:50:05) by gitwatch.sh

---
+++ 2026-09-25/CheckForDups.kt
@@ -0,0 +1,12 @@
+// Look for duplicate values
+
+fun checkForDups(list: List<Int>): Boolean {
+    for (i in 0 ..< list.count()) {
+        for (j in 0 ..< list.count()) {
+            if (list[i] == list[j] && i != j) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
